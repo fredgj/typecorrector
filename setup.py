99d7b503f65ba4c9b77324e2d624ec715908c380
fredgj <f.gjertsen@gmail.com>
2015-07-30 02:16:17 +0200

    updated version number

--- setup.py
+++ setup.py
@@ -2,10 +2,9 @@ from setuptools import setup
 
 __author__ = 'Fredrik Gjertsen'
 __doc__ = open('README.rst').read()
-__description__ = 'A decorator meant to give hints about a functions parameter types as well as allow a margin of error'
-
+__description__ = 'A decorator meant to give type hints about function parameters as well as allow a margin of error'
 setup(name='typecorrector',
-      version='0.1.0',
+      version='0.2.1',
       description=__description__,
       long_description=__doc__,
       author=__author__,
